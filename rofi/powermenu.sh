e1278f45ea0f192d98fb654f0c4296faac7636fa
Saumya Karan <contact@saumyakaran.com>
2026-04-11 02:17:09 +0530

    feat: add catppuccin macchiato swaylock theme

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -3,7 +3,7 @@
 chosen=$(echo -e '<span color="#8aadf4">󰒲</span>  Sleep\n<span color="#a6da95">󰍃</span>  Logout\n<span color="#f5a97f">󰜉</span>  Reboot\n<span color="#ed8796">󰐥</span>  Shutdown' | rofi -dmenu -p "" -markup-rows -theme ~/dotfiles/rofi/powermenu.rasi)
 
 case "$chosen" in
-    *Sleep*) ssh-add -D; swaylock -c 1e2030 && systemctl suspend ;;
+    *Sleep*) ssh-add -D; swaylock && systemctl suspend ;;
     *Logout*) swaymsg exit ;;
     *Reboot*) systemctl reboot ;;
     *Shutdown*) systemctl poweroff ;;
